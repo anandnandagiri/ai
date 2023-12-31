d04bd16bee17dc1f5427542cd6d07875c12a7e2e
Anand <pavan.anand.n@gmail.com>
2023-12-31 16:09:19 +0530

    added few model concepts on Notes

--- js/history.js
+++ js/history.js
@@ -1,13 +1,15 @@
 
 var tableData = [
-    {"td1": "Nov 2022", "td2": "ChatGPT-3", "td3": "ChatGPT hit 1 Million in a Week" },
+    { "td1": "Nov 2022", "td2": "ChatGPT-3", "td3": "ChatGPT Initial Version is released and it hit 1 Million in a Week" },    
     {"td1": "Feb 2023", "td2": "LLaMA", "td3": "<b>Released By: </b> META (Facebook) <br/><b>Release Date: </b> 23 Feb 2023 <br/> 7B,13B,33B and 60B <br>Parameters 1.4 Trillion Token" },
     {"td1": "July 2023", "td2": "LLaMA-2", "td3": "<b>Released By: </b> META (Facebook) <br/><b>Release Date: </b>18th July 2023 <br/>7B,13B and 70B <br/>Parameters 2 Trillion Token" },
-    {"td1": "Aug 2023", "td2": "GGUF", "td3": "GGUF Latest File Format for AI Model which is successor to GGML" },
-
+    { "td1": "July 2023", "td2": "ollama", "td3": "ollama v0.0.1 This is an early preview release of Ollama" },
+    { "td1": "Aug 2023", "td2": "GGUF", "td3": "GGUF Latest File Format for AI Model which is successor to GGML" },
+    { "td1": "Nov 2023", "td2": "ChatGPT-3", "td3": "ChatGPT Stable Version is released" },
+    { "td1": "Dev 2023", "td2": "ollama", "td3": "ollama v0.1.117 supports most of models like orca-2,Phi-2,mixtral. Multimodal Model " },
     //{"td1":"","td2":"","td3":""},
     //{"td1":"","td2":"","td3":""},
 ];
 
 
-//="{""td1"":""" & G7 & """,""td2"":""" & H7 & """,""td3:""" & I7 & """},"
+//="{""td1"":""" & A1 & """,""td2"":""" & B1 & """,""td3:""" & C1 & """},"
---
+++ js/notes.js
@@ -0,0 +1,11 @@
+
+
+
+var mData = [
+    { "td1": "Model Size", "td2": "The overall size of the model, often measured in terms of parameters (e.g., 7B for 7 billion parameters)." },
+    { "td1": "Training Tokens", "td2": "The number of tokens in the training dataset, indicating the scale of the data used for model training (e.g., 1.4T for 1.4 trillion tokens)." },
+    { "td1": "Context Length", "td2": "The maximum number of tokens considered as context during inference or generation (e.g., 2048 tokens)." },
+    { "td1": "Architecture", "td2": "The underlying structure and design of the model, such as transformer-based architectures like GPT (Generative Pre-trained Transformer) or BERT (Bidirectional Encoder Representations from Transformers)." },
+    { "td1": "Tokenization", "td2": "The method used to break down text into tokens, which can be words, subwords, or characters, depending on the chosen strategy." },
+    { "td1": "Training Data", "td2": "Information about the source and diversity of the training dataset, providing insights into the model's exposure to different language patterns and contexts." },
+];
