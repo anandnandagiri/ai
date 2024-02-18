3ed2833eb8fef9fcda6cbde8516c9418973ca750
VAapps <53390746+VAapps@users.noreply.github.com>
2024-02-18 12:51:11 +0530

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -13,7 +13,6 @@ var sliderData = {
                 { "label": "Langsmith", "url": "https://smith.langchain.com/" },
                 { "label": "Gradient CLI Fine Tune Model", "url": "https://www.gradient.ai/" },
                 { "label": "Executable Model with llamafile", "url": "https://github.com/Mozilla-Ocho/llamafile" },
-                { "label": "Ollama Modelfiles", "url": "https://openwebui.com/" },
                  //{ "label": "", "url": "" },
                  //{ "label": "", "url": "" },
                 //{ "label": "", "url": "" },
