618e60a580f69de98571a2301e7210c57190d74e
anandnandagiri <pavan.anand.n@gmail.com>
2024-11-22 23:59:26 +0530

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -59,6 +59,7 @@ var sliderData = {
         {
             "title": "GitHub - Source Code",
             "links": [
+                { "label": "Exo Run Your Own AI Cluster", "url": "https://github.com/exo-explore/exo" },
                  { "label": "OpenVINO Toolkit", "url": "https://github.com/openvinotoolkit" },
                 { "label": "unsloth - Finetune Models", "url": "https://github.com/unslothai/unsloth" },
                 { "label": "Vanna AI RAG Framework for SQL Generation", "url": "https://github.com/vanna-ai/vanna" },
@@ -74,6 +75,8 @@ var sliderData = {
                 {"label": "CodeLLAMA","url": "https://github.com/facebookresearch/codellama/tree/main"},
                 {"label": "LLaVa 1.6","url": "https://github.com/haotian-liu/LLaVA"},
                 {"label": "LLMware", "url": "https://github.com/llmware-ai/llmware"},               
+                //{ "label": "", "url": "" },
+                //{ "label": "", "url": "" },
                 //{ "label": "", "url": "" },
                 //{ "label": "", "url": "" },
             ]
